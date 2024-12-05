5ec51aed467bc7f79bbf33b4320112dcd3e069bc
Satrio Wicaksono <>
2024-12-05 16:09:32 +0700

    Initiated project

---
+++ swiftui firestore/swiftui firestore/swiftui_firestoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_firestoreApp.swift
+//  swiftui firestore
+//
+//  Created by Satrio Wicaksono on 05/12/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_firestoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
